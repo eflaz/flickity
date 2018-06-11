4a6b201a2ba3418a99961aaa0d8c6edecf9e7fc2
Ewelina Kusmierczyk-Flazinska <ewelina@flazinska.pl>
2018-06-11 10:20:55 +0200

    add restart button

--- js/script.js
+++ js/script.js
@@ -7,9 +7,15 @@ var flkty = new Flickity( elem, {
   hash: true,
 });
 
-// element argument can be a selector string
-//   for an individual element
-var flkty = new Flickity( '.main-carousel', {
-  // options
+var buttonGroup = document.querySelector('.button-group');
+var buttons = buttonGroup.querySelectorAll('.button');
+buttons = fizzyUIUtils.makeArray( buttons );
 
+buttonGroup.addEventListener( 'click', function( event ) {
+  // filter for button clicks
+  if ( !matchesSelector( event.target, '.button' ) ) {
+    return;
+  }
+  var index = buttons.indexOf( event.target );
+  flkty.select( index );
 });
