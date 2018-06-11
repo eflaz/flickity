0e20b8d3ad103653b0c0c2a228b3f2c663bfbf63
Ewelina Kusmierczyk-Flazinska <ewelina@flazinska.pl>
2018-06-11 18:00:26 +0200

    fixed restart button

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ var flkty = new Flickity( elem, {
   hash: true,
 });
 
-var buttonGroup = document.querySelector('.button-group');
+/*var buttonGroup = document.querySelector('.button-group');
 var buttons = buttonGroup.querySelectorAll('.button');
 buttons = fizzyUIUtils.makeArray( buttons );
 
@@ -17,5 +17,26 @@ buttonGroup.addEventListener( 'click', function( event ) {
     return;
   }
   var index = buttons.indexOf( event.target );
-  flkty.select( index );
-});
+  flkty.select( index );*/
+
+
+
+function putResetButtons(){
+  var slides = document.querySelectorAll('.carousel-cell');
+  var buttonHtml = '<button class="button">RESTART</button>';
+  for (var i = 0; i < slides.length; i++) {
+  	slides[i].innerHTML = buttonHtml + slides[i].innerHTML;
+  }
+}
+
+function setResetActionForButtons() {
+	var buttons = document.querySelectorAll('.button');
+	for (var i = 0; i < buttons.length; i++) {
+		buttons[i].addEventListener('click', function(event) {
+	  	flkty.select(0, true, false);
+		});
+	}
+}
+
+putResetButtons();
+setResetActionForButtons();
