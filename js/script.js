0396be82281b8a6c0cea8d5481926e3b0f704679
Ewelina Kusmierczyk-Flazinska <ewelina@flazinska.pl>
2018-06-12 23:22:41 +0200

    fixed restart button

--- js/script.js
+++ js/script.js
@@ -1,49 +1,17 @@
 var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
-  // options
-  cellAlign: 'left',
-  contain: true,
-  pageDots: false,
-  hash: true,
+var flkty = new Flickity(elem, {
+	// options
+	cellAlign: 'left',
+	contain: true,
+	pageDots: false,
+	hash: true,
 });
 var progressBar = document.querySelector('.progress-bar')
-
-flkty.on( 'scroll', function( progress ) {
-  progress = Math.max( 0, Math.min( 1, progress ) );
-  progressBar.style.width = progress * 100 + '%';
+flkty.on('scroll', function(progress) {
+	progress = Math.max(0, Math.min(1, progress));
+	progressBar.style.width = progress * 100 + '%';
 });
-
-
-/*var buttonGroup = document.querySelector('.button-group');
-var buttons = buttonGroup.querySelectorAll('.button');
-buttons = fizzyUIUtils.makeArray( buttons );
-
-buttonGroup.addEventListener( 'click', function( event ) {
-  // filter for button clicks
-  if ( !matchesSelector( event.target, '.button' ) ) {
-    return;
-  }
-  var index = buttons.indexOf( event.target );
-  flkty.select( index );*/
-
-
-
-/*function putResetButtons(){
-  var slides = document.querySelectorAll('.carousel-cell');
-  var buttonHtml = '<button class="button">RESTART</button>';
-  for (var i = 0; i < slides.length; i++) {
-  	slides[i].innerHTML = buttonHtml + slides[i].innerHTML;
-  }
-}
-
-function setResetActionForButtons() {
-	var buttons = document.querySelectorAll('.button');
-	for (var i = 0; i < buttons.length; i++) {
-		buttons[i].addEventListener('click', function(event) {
-	  	flkty.select(0, true, false);
-		});
-	}
-}
-
-putResetButtons();
-setResetActionForButtons();*/
+var restartButton = document.getElementById('button-restart');
+restartButton.addEventListener("click", function() {
+	flkty.select(0);
+});
