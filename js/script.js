81d951755bc731831955fa294a0d3b1e2eacd4c2
Ewelina Kusmierczyk-Flazinska <ewelina@flazinska.pl>
2018-06-15 00:23:29 +0200

    add active markers

--- js/script.js
+++ js/script.js
@@ -1,15 +1,13 @@
 'use strict';
-(function(){
+(function() {
 	var templateSlide = document.getElementById('template-slide').innerHTML;
 	var mainCarousel = document.querySelector('.main-carousel');
-
 	Mustache.parse(templateSlide);
 	var listItems = '';
-	for(var i = 0; i < slidesData.length; i++) {
-			listItems += Mustache.render(templateSlide, slidesData[i]);
-		} 
+	for (var i = 0; i < slidesData.length; i++) {
+		listItems += Mustache.render(templateSlide, slidesData[i]);
+	}
 	mainCarousel.insertAdjacentHTML('beforeend', listItems);
-
 	var elem = document.querySelector('.main-carousel');
 	var flkty = new Flickity(elem, {
 		// options
@@ -18,28 +16,48 @@
 		pageDots: false,
 		hash: true,
 	});
-
 	var progressBar = document.querySelector('.progress-bar');
 	flkty.on('scroll', function(progress) {
 		progress = Math.max(0, Math.min(1, progress));
 		progressBar.style.width = progress * 100 + '%';
 	});
 
+
 	var restartButton = document.getElementById('button-restart');
 	restartButton.addEventListener("click", function() {
 		flkty.select(0);
 	});
 
-	window.initMap = function(){
-	  var map = new google.maps.Map(
-	      document.getElementById('map'), {zoom: 2, center: slidesData[0].coords});
+	var markerClicked = false;
+	window.initMap = function() {
+		var map = new google.maps.Map(document.getElementById('map'), {
+			zoom: 2,
+			center: slidesData[0].coords
+		});
+		var marker = '';
+		var markers = [];
+		var addListener = function(i) {
+			google.maps.event.addListener(markers[i], 'click', function() {
+				markerClicked = true;
+				flkty.select(i);
+			});
+		};
+		for (var i = 0; i < slidesData.length; i++) {
+			marker = new google.maps.Marker({
+				position: slidesData[i].coords,
+				map: map
+			});
+			markers.push(marker);
+			addListener(i);
+		}
 
-	  var marker = '';
-	  for (var i = 0; i < slidesData.length; i++) {
-	  	marker = new google.maps.Marker({position: slidesData[i].coords, map: map});
-	  }
-	};
-
+		flkty.on('change', function(index){
+			if (markerClicked == false) {
+				map.panTo(slidesData[index].coords);
+			}
+			markerClicked = false;
+		});
+	};	
 
 })();
 
