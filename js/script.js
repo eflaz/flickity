fbe8b2106fb623e3989446d13f989b3c75e9f107
Ewelina Kusmierczyk-Flazinska <ewelina@flazinska.pl>
2018-06-15 22:28:43 +0200

    fixed tabs in js and html

--- js/script.js
+++ js/script.js
@@ -1,16 +1,14 @@
 'use strict';
-(function(){
+(function() {
 	var templateSlide = document.getElementById('template-slide').innerHTML;
 	var mainCarousel = document.querySelector('.main-carousel');
-
 	Mustache.parse(templateSlide);
 	var listItems = '';
-	for(var i = 0; i < slidesData.length; i++) {
-			console.log(slidesData);
-			listItems += Mustache.render(templateSlide, slidesData[i]);
-		} 
+	for (var i = 0; i < slidesData.length; i++) {
+		console.log(slidesData);
+		listItems += Mustache.render(templateSlide, slidesData[i]);
+	}
 	mainCarousel.insertAdjacentHTML('beforeend', listItems);
-
 	var elem = document.querySelector('.main-carousel');
 	var flkty = new Flickity(elem, {
 		// options
@@ -19,16 +17,13 @@
 		pageDots: false,
 		hash: true,
 	});
-
 	var progressBar = document.querySelector('.progress-bar');
 	flkty.on('scroll', function(progress) {
 		progress = Math.max(0, Math.min(1, progress));
 		progressBar.style.width = progress * 100 + '%';
 	});
-
 	var restartButton = document.getElementById('button-restart');
 	restartButton.addEventListener("click", function() {
 		flkty.select(0);
 	});
-
 })();
